587e3da38ce24873e9feed2ddae9576f4a015697
Dee <slawotsky@gmail.com>
2023-01-01 16:38:54 -0500

    begin front end work

---
+++ clock.js
@@ -0,0 +1,15 @@
+function startTime() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('clock').innerHTML =  h + ":" + m + ":" + s;
+    setInterval(startTime, 1000);
+  }
+  
+  function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+  }
