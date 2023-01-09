34a3bf699b831382e35041684781ec097c831e2c
Dee <slawotsky@gmail.com>
2023-01-08 21:58:46 -0500

    Added animation to search bar

---
+++ scriptSearch.js
@@ -0,0 +1,38 @@
+// ALL OF THIS CODE IS RELATED TO SEARCH BAR
+
+console.log("test");
+
+
+let openSearchButton = document.getElementById("open-search-button");
+let searchBarArea = document.getElementById("search-bar-area");
+let enterSearchButton = document.getElementById("enter-search-button");
+let inputField = document.getElementById("search-box");
+enterSearchButton.style.visibility = 'hidden';
+inputField.style.width = 0;
+console.log(searchBarArea);
+
+// Once you click the circle search icon
+openSearchButton.addEventListener('click', () => {
+    enterSearchButton.style.visibility = 'visible';
+    inputField.style.visibility = 'visible';
+    inputField.style.width = '200px'; // Change width so animation will appear
+
+    setTimeout(function () {
+        document.getElementById("selected-cities").remove();
+        document.getElementById("open-search-button").remove();
+    }, 1000); // Wait 1 second for animation to finish before removing text
+
+    // Submitting whatever you have in input field
+    enterSearchButton.addEventListener('click', () => {
+        let locValue = inputField.value;
+        console.log(locValue);
+    });
+
+    // Entering input field
+    inputField.addEventListener('keyup', (e) => {
+        if(e.keyCode == 13 && inputField.value!=``) {
+            let locValue = inputField.value;
+            console.log(locValue);
+        }
+    });
+});
