e84633982e24d7143873268e45c946b8d8fd4e0c
Dee <slawotsky@gmail.com>
2023-01-13 19:21:28 -0500

    Added clock to site

--- clock.js
+++ clock.js
@@ -1,15 +1,13 @@
-function startTime() {
-    const today = new Date();
-    let h = today.getHours();
-    let m = today.getMinutes();
-    let s = today.getSeconds();
-    m = checkTime(m);
-    s = checkTime(s);
-    document.getElementById('clock').innerHTML =  h + ":" + m + ":" + s;
-    setInterval(startTime, 1000);
-  }
-  
-  function checkTime(i) {
-    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
-    return i;
-  }
+var clock = document.getElementById('clock');
+
+setInterval(function() {
+
+    var currentTime = new Date(),
+    hours = currentTime.getHours(),
+    minutes = currentTime.getMinutes(),
+    ampm = hours > 11 ? 'PM' : 'AM';
+    hours = hours > 12 ? hours-12 : hours;
+    minutes += minutes < 10 ? '0' : '';
+
+    clock.innerHTML = hours + ":" + minutes + " " + ampm;
+}, 1000);
